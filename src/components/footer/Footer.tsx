f0de403aeb63d06298ab3259bd3ccbec9411f5d0
wisdomfire <468179220@qq.com>
2023-09-19 23:21:36 +0800

    'next0.1'

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>©2023 Lamamia. All rights reserved.</div>
+      <div></div>
+    </div>
+
+  )
+}
+
+export default Footer
